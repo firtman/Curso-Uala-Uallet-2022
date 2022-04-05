7dce8e14670f05bdd8d7e9cf4889f25d94bdee97
Maximiliano Firtman <firtman@gmail.com>
2022-04-05 11:51:25 -0300

    Tableview básico que muestra las wallets

--- Uallet/ViewControllers/WalletsViewController.swift
+++ Uallet/ViewControllers/WalletsViewController.swift
@@ -2,41 +2,101 @@
 //  WalletsViewController.swift
 //  Uallet
 //
-//  Created by Maximiliano Firtman on 3/30/22.
+//  Created by Maximiliano Firtman on 4/5/22.
 //
 
 import UIKit
 
-
 class WalletsViewController: UITableViewController {
- 
+    
+    let cellIdentifier = "celdaWallets"
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        self.clearsSelectionOnViewWillAppear = false
+
         title = "Wallets"
-        
-        
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWallet))
+        
+        
+        // Registrar una celda custom
+        let nib = UINib(nibName: "WalletCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
+        
     }
-
+    
     @objc func addWallet() {
-        let addVC = WalletAddViewController()
-        present(UINavigationController(rootViewController: addVC), animated: true)
-        
-                
+            let addVC = WalletAddViewController()
+            present(UINavigationController(rootViewController: addVC), animated: true)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return WalletsStorage.shared.wallets.count
     }
 
     
-    
-    fileprivate func crearElementosManual() {
-        let stack = UIStackView(frame: CGRect(x: 15, y: 80, width: 300, height: 300))
-        stack.axis = .vertical
-        self.view.addSubview(stack)
-        
-        for wallet in WalletsStorage.shared.wallets {
-            let label = UILabel()
-            label.text = wallet.name
-            stack.addSubview(label)
-        }
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+                                                 for: indexPath)
+        let wallet = WalletsStorage.shared.wallets[indexPath.row]
+        cell.textLabel!.text = wallet.name
+            
+        return cell
     }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
 }
